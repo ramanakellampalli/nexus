495ebbc0d248baec2c23397c8e2dfadae7fe9fe0
Ramana <ramanakellampalli@gmail.com>
2025-10-01 19:28:34 -0500

    spotless checks

--- src/main/java/com/ohyeahsaas/nexus/config/SecurityConfig.java
+++ src/main/java/com/ohyeahsaas/nexus/config/SecurityConfig.java
@@ -16,8 +16,11 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             auth ->
                 auth.requestMatchers(
-                        "/actuator/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**",
-                                "api/v1/ping")
+                        "/actuator/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "api/v1/ping")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
